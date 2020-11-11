119f91bc2e8f0039c8f94b3dfbfd534eed4fb8ef
MaPaulaMV <ma.paulamonteros@gmail.com>
2020-11-10 21:13:27 -0500

    HTTP part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -22,6 +22,7 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next: string;
   newComment: Comment;
+  errMess: string;
 
   @ViewChild('fform') commentsFormDirective;
   commentsForm: FormGroup;
@@ -112,7 +113,8 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice
       .getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+      errmess => this.errMess = <any>errmess);
   }
 
   setPrevNext(dishId: string) {
